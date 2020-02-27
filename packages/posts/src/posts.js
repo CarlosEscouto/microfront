c6507399ac3a4fdf600fc8a6f5fe46416e7954de
Carlos Henrique Escouto <carloshenriqueescouto@gmail.com>
2020-02-27 09:09:21 -0300

    Init repo

---
+++ packages/posts/src/posts.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import singleSpaVue from 'single-spa-vue';
+
+Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    el: '#posts',
+    render: r => r(App)
+  } 
+});
+
+export const bootstrap = [
+  vueLifecycles.bootstrap,
+];
+
+export const mount = [
+  vueLifecycles.mount,
+];
+
+export const unmount = [
+  vueLifecycles.unmount,
+];
